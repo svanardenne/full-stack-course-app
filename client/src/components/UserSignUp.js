87c587ca2195ec88948a6cf330cddc6bc5ff2713
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-02-03 16:15:23 -0800

    Added fillable form to UserSignUp

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -2,9 +2,28 @@ import React, { Component } from 'react';
 
 class UserSignUp extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      firstName: '',
+      lastName: '',
+      emailAddress: '',
+      password: '',
+      confirmPassword: ''
+    }
+  }
+
   cancel = (event) => {
     event.preventDefault();
     this.props.history.push('/');
+  }
+
+  handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
   }
 
   render() {
@@ -14,13 +33,26 @@ class UserSignUp extends Component {
           <h1>Sign Up</h1>
           <div>
             <form>
-              <div><input id="firstName" name="firstName" type="text" className="" placeholder="First Name" value="" /></div>
-              <div><input id="lastName" name="lastName" type="text" className="" placeholder="Last Name" value="" /></div>
-              <div><input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" value="" /></div>
-              <div><input id="password" name="password" type="password" className="" placeholder="Password" value="" /></div>
-              <div><input id="confirmPassword" name="confirmPassword" type="password" className="" placeholder="Confirm Password"
-                  value="" /></div>
-              <div className="grid-100 pad-bottom"><button className="button" type="submit">Sign Up</button><button className="button button-secondary" onClick={this.cancel}>Cancel</button></div>
+              <div>
+                <input id="firstName" name="firstName" type="text" className="" placeholder="First Name" value={this.state.firstName} onChange={this.handleChange} />
+              </div>
+              <div>
+                <input id="lastName" name="lastName" type="text" className="" placeholder="Last Name" value={this.state.lastName} onChange={this.handleChange} />
+              </div>
+              <div>
+                <input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" value={this.state.emailAddress} onChange={this.handleChange} />
+              </div>
+              <div>
+                <input id="password" name="password" type="password" className="" placeholder="Password" value={this.state.password} onChange={this.handleChange} />
+              </div>
+              <div>
+                <input id="confirmPassword" name="confirmPassword" type="password" className="" placeholder="Confirm Password"
+                  value={this.state.confirmPassword} onChange={this.handleChange} />
+              </div>
+              <div className="grid-100 pad-bottom">
+                <button className="button" type="submit">Sign Up</button>
+                <button className="button button-secondary" onClick={this.cancel}>Cancel</button>
+              </div>
             </form>
           </div>
           <p>&nbsp;</p>
