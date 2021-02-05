757870ea02925e8122c99fd57b2abf0ca3a8d80a
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-02-05 13:01:55 -0800

    Added state to the updateCourse component.  Retrieved data to pre-fill values in input boxes on UpdateCourse

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class UpdateCourse extends Component {
 
   state = {
-    title: '',
-    description: '',
-    estimatedTime: '',
-    materialsNeeded: ''
+    data: {},
+    user: {}
+  }
+
+  componentDidMount() {
+    // Gets course data for a specifc course and adds it to state
+    axios.get(`http://localhost:5000/api/courses/${this.props.match.params.id}`)
+      .then(data => this.setState({data: data.data, user: data.data.User}));
+  }
+
+  handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      data: {
+        [name]: value
+      }
+    });
   }
 
   cancel = (event) => {
@@ -24,11 +39,12 @@ class UpdateCourse extends Component {
             <div className="course--header">
               <h4 className="course--label">Course</h4>
               <div><input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..."
-                  value="Build a Basic Bookcase" /></div>
+                  value={this.state.data.title} onChange={this.handleChange} /></div>
               <p>By Joe Smith</p>
             </div>
             <div className="course--description">
-              <div><textarea id="description" name="description" className="" placeholder="Course description..."></textarea></div>
+              <div><textarea id="description" name="description" className="" placeholder="Course description..." 
+                value={this.state.data.description} onChange={this.handleChange}></textarea></div>
             </div>
           </div>
           <div className="grid-25 grid-right">
@@ -37,11 +53,12 @@ class UpdateCourse extends Component {
                 <li className="course--stats--list--item">
                   <h4>Estimated Time</h4>
                   <div><input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input"
-                      placeholder="Hours" value="14 hours" /></div>
+                      placeholder="Hours" value={this.state.data.estimatedTime} onChange={this.handleChange} /></div>
                 </li>
                 <li className="course--stats--list--item">
                   <h4>Materials Needed</h4>
-                  <div><textarea id="materialsNeeded" name="materialsNeeded" className="" placeholder="List materials..."></textarea></div>
+                  <div><textarea id="materialsNeeded" name="materialsNeeded" className="" placeholder="List materials..." 
+                    value={this.state.data.materialsNeeded} onChange={this.handleChange}></textarea></div>
                 </li>
               </ul>
             </div>
