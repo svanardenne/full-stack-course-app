d59bcb074a194f3f5a1251a6b675a6a3e45cf894
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-02-09 09:22:03 -0800

    Added Forbidden and Error components

---
+++ client/src/components/Error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Err = () => {
+  return(
+    <div class="bounds">
+      <h1>Error</h1>
+      <p>Sorry! We just encountered an unexpected error.</p>
+    </div>
+  );
+}
+
+export default Err;
