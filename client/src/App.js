7fae8fd8ddb241002dec963fcd92f41711efdabf
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-02-19 13:30:48 -0800

    Added base path

--- client/src/App.js
+++ client/src/App.js
@@ -36,21 +36,21 @@ const CourseDetailWithContext = withContext(CourseDetail);
 
 const App = () => {
   return(
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <div className="App">
         <HeaderWithContext />
         <hr/>
         <Switch>
           <Route exact path="/" component={CoursesWithContext} />
-          <PrivateRoute path="/courses/create" component={CreateCourseWithContext} />
-          <PrivateRoute path="/courses/:id/update" component={UpdateCourseWithContext} />
+          <PrivateRoute exact path="/courses/create" component={CreateCourseWithContext} />
+          <PrivateRoute exact path="/courses/:id/update" component={UpdateCourseWithContext} />
           <Route exact path="/courses/:id" component={CourseDetailWithContext} />
-          <Route path="/signin" component={UserSignInWithContext} />
-          <Route path="/signup" component={UserSignUpWithContext} />
-          <Route path="/signout" component={UserSignOutWithContext} />
-          <Route path="/notfound" component={NotFound} />
-          <Route path="/forbidden" component={Forbidden} />
-          <Route path="/error" component={UnhandledError} />
+          <Route exact path="/signin" component={UserSignInWithContext} />
+          <Route exact path="/signup" component={UserSignUpWithContext} />
+          <Route exact path="/signout" component={UserSignOutWithContext} />
+          <Route exact path="/notfound" component={NotFound} />
+          <Route exact path="/forbidden" component={Forbidden} />
+          <Route exact path="/error" component={UnhandledError} />
           <Route component={NotFound} />
         </Switch>
       </div>
