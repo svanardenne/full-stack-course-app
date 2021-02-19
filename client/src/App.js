3afbd732d52e6ea6a74212ba01c12ed78a533796
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-02-19 13:31:22 -0800

    Added base path

--- client/src/App.js
+++ client/src/App.js
@@ -42,15 +42,15 @@ const App = () => {
         <hr/>
         <Switch>
           <Route exact path="/" component={CoursesWithContext} />
-          <PrivateRoute exact path="/courses/create" component={CreateCourseWithContext} />
-          <PrivateRoute exact path="/courses/:id/update" component={UpdateCourseWithContext} />
+          <PrivateRoute path="/courses/create" component={CreateCourseWithContext} />
+          <PrivateRoute path="/courses/:id/update" component={UpdateCourseWithContext} />
           <Route exact path="/courses/:id" component={CourseDetailWithContext} />
-          <Route exact path="/signin" component={UserSignInWithContext} />
-          <Route exact path="/signup" component={UserSignUpWithContext} />
-          <Route exact path="/signout" component={UserSignOutWithContext} />
-          <Route exact path="/notfound" component={NotFound} />
-          <Route exact path="/forbidden" component={Forbidden} />
-          <Route exact path="/error" component={UnhandledError} />
+          <Route path="/signin" component={UserSignInWithContext} />
+          <Route path="/signup" component={UserSignUpWithContext} />
+          <Route path="/signout" component={UserSignOutWithContext} />
+          <Route path="/notfound" component={NotFound} />
+          <Route path="/forbidden" component={Forbidden} />
+          <Route path="/error" component={UnhandledError} />
           <Route component={NotFound} />
         </Switch>
       </div>
