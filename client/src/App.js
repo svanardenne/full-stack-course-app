421c9d33aa313b33afd338a2ea994229f50c6769
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-03-12 15:15:17 -0800

    Added hash router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Switch
 } from 'react-router-dom';
@@ -36,7 +36,7 @@ const CourseDetailWithContext = withContext(CourseDetail);
 
 const App = () => {
   return(
-    <Router basename="/full-stack-course-app">
+    <Router>
       <div className="App">
         <HeaderWithContext />
         <hr/>
