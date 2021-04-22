108b444b801b8ab1bb8d043ab491245cfb9891a5
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-04-22 12:31:08 -0700

    Finished styled components for mobile screens

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const FormWrapper = styled.div`
+  @media (max-width: 1000px) {
+    width: 100%;
+  }
+`;
 
 class UserSignIn extends Component {
-
   constructor() {
     super();
     this.state = {
-      emailAddress: '',
-      password: '',
-      errors: []
-    }
+      emailAddress: "",
+      password: "",
+      errors: [],
+    };
   }
 
   // Handles value change in input fields
@@ -17,38 +23,40 @@ class UserSignIn extends Component {
     const name = event.target.name;
     const value = event.target.value;
     this.setState({
-      [name]: value
+      [name]: value,
     });
-  }
+  };
 
   // Submit Handler
   submit = (event) => {
     event.preventDefault();
     const { context } = this.props;
-    const { from } = this.props.location.state || { from: { pathname: '/' } }
+    const { from } = this.props.location.state || { from: { pathname: "/" } };
     const { emailAddress, password } = this.state;
-    context.actions.signIn(emailAddress, password)
-      .then(user => {
+    context.actions
+      .signIn(emailAddress, password)
+      .then((user) => {
         if (user === null) {
           this.setState(() => {
-            return { errors: [ 'Sign-in was unsuccessful' ] };
+            return { errors: ["Sign-in was unsuccessful"] };
           });
         } else {
           this.props.history.push(from);
           console.log(`SUCCESS! ${emailAddress} is now signed in!`);
         }
       })
-      .catch(err => { // Handle rejected promises
+      .catch((err) => {
+        // Handle rejected promises
         console.log(err);
-        this.props.history.push('/error'); // push error to history stack
+        this.props.history.push("/error"); // push error to history stack
       });
-  }
+  };
 
   // Cancel button handler
   cancel = (event) => {
     event.preventDefault();
-    this.props.history.push('/');
-  }
+    this.props.history.push("/");
+  };
 
   // Displays validation errors
   ErrorsDisplay = ({ errors }) => {
@@ -59,43 +67,69 @@ class UserSignIn extends Component {
           <h2 className="validation--errors--label">Validation errors</h2>
           <div className="validation-errors">
             <ul>
-              {errors.map((error, i) => <li key={i}>{error}</li>)}
+              {errors.map((error, i) => (
+                <li key={i}>{error}</li>
+              ))}
             </ul>
           </div>
         </div>
-      )
+      );
     }
     return errorsDisplay;
-  }
+  };
 
   render() {
-    return(
+    return (
       <div className="bounds">
-        <div className="grid-33 centered signin">
+        <FormWrapper className="grid-33 centered signin">
           <h1>Sign In</h1>
           <div>
             <form onSubmit={this.submit}>
               <this.ErrorsDisplay errors={this.state.errors} />
               <div>
-                <input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" 
-                  value={this.state.emailAddress} onChange={this.handleChange} />
+                <input
+                  id="emailAddress"
+                  name="emailAddress"
+                  type="text"
+                  className=""
+                  placeholder="Email Address"
+                  value={this.state.emailAddress}
+                  onChange={this.handleChange}
+                />
               </div>
               <div>
-                <input id="password" name="password" type="password" className="" placeholder="Password" 
-                  value={this.state.password} onChange={this.handleChange} />
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  className=""
+                  placeholder="Password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                />
               </div>
               <div className="grid-100 pad-bottom">
-                <button className="button" type="submit">Sign In</button>
-                <button className="button button-secondary" onClick={this.cancel}>Cancel</button>
+                <button className="button" type="submit">
+                  Sign In
+                </button>
+                <button
+                  className="button button-secondary"
+                  onClick={this.cancel}
+                >
+                  Cancel
+                </button>
               </div>
             </form>
           </div>
           <p>&nbsp;</p>
-          <p>Don't have a user account? <Link to="/signup">Click here</Link> to sign up!</p>
-        </div>
+          <p>
+            Don't have a user account? <Link to="/signup">Click here</Link> to
+            sign up!
+          </p>
+        </FormWrapper>
       </div>
     );
-  };
+  }
 }
 
 export default UserSignIn;
--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -1,73 +1,84 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const FormWrapper = styled.div`
+  @media (max-width: 1000px) {
+    width: 100%;
+  }
+`;
+const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  @media (max-width: 330px) {
+    button {
+      font-size: 1em;
+    }
+  }
+`;
 
 class UserSignUp extends Component {
-
   state = {
-      firstName: '',
-      lastName: '',
-      emailAddress: '',
-      password: '',
-      confirmPassword: '',
-      errors: []
-    }
+    firstName: "",
+    lastName: "",
+    emailAddress: "",
+    password: "",
+    confirmPassword: "",
+    errors: [],
+  };
 
   // Handles value change in input fields
   handleChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
     this.setState({
-      [name]: value
+      [name]: value,
     });
-  }
+  };
 
   // Submit Handler
   submit = (event) => {
     event.preventDefault();
     const { context } = this.props;
-    if(this.state.password === this.state.confirmPassword) {
-      const {
-        firstName,
-        lastName,
-        emailAddress,
-        password
-      } = this.state;
+    if (this.state.password === this.state.confirmPassword) {
+      const { firstName, lastName, emailAddress, password } = this.state;
       const user = {
         firstName,
         lastName,
         emailAddress,
-        password
+        password,
       };
       // Calls createUser() from data passed to context
-      context.userData.createUser(user)
-        .then( errors => {
+      context.userData
+        .createUser(user)
+        .then((errors) => {
           if (errors.length) {
             this.setState({ errors });
           } else {
             // Signs new user in and redirects them to main page
-            context.actions.signIn(emailAddress, password)
-              .then(() => {
-                this.props.history.push('/');
-              });
+            context.actions.signIn(emailAddress, password).then(() => {
+              this.props.history.push("/");
+            });
           }
         })
-        .catch(err => { // Handle rejected promises
+        .catch((err) => {
+          // Handle rejected promises
           console.log(err);
-          this.props.history.push('/error'); // push error to history stack
+          this.props.history.push("/error"); // push error to history stack
         });
     } else {
-      this.setState({errors: [
-        ...this.state.errors,
-        "Passwords need to match"
-      ]});
+      this.setState({
+        errors: [...this.state.errors, "Passwords need to match"],
+      });
     }
-  }
+  };
 
   // Cancel button handler
   cancel = (event) => {
     event.preventDefault();
-    this.props.history.push('/');
-  }
+    this.props.history.push("/");
+  };
 
   // Displays validation errors
   ErrorsDisplay = ({ errors }) => {
@@ -78,51 +89,102 @@ class UserSignUp extends Component {
           <h2 className="validation--errors--label">Validation errors</h2>
           <div className="validation-errors">
             <ul>
-              {errors.map((error, i) => <li key={i}>{error}</li>)}
+              {errors.map((error, i) => (
+                <li key={i}>{error}</li>
+              ))}
             </ul>
           </div>
         </div>
-      )
+      );
     }
     return errorsDisplay;
-  }
+  };
 
   render() {
-    return(
+    return (
       <div className="bounds">
-        <div className="grid-33 centered signin">
+        <FormWrapper className="grid-33 centered signin">
           <h1>Sign Up</h1>
           <div>
-          <this.ErrorsDisplay errors={this.state.errors} />
+            <this.ErrorsDisplay errors={this.state.errors} />
             <form onSubmit={this.submit}>
               <div>
-                <input id="firstName" name="firstName" type="text" className="" placeholder="First Name" value={this.state.firstName} onChange={this.handleChange} />
+                <input
+                  id="firstName"
+                  name="firstName"
+                  type="text"
+                  className=""
+                  placeholder="First Name"
+                  value={this.state.firstName}
+                  onChange={this.handleChange}
+                />
               </div>
               <div>
-                <input id="lastName" name="lastName" type="text" className="" placeholder="Last Name" value={this.state.lastName} onChange={this.handleChange} />
+                <input
+                  id="lastName"
+                  name="lastName"
+                  type="text"
+                  className=""
+                  placeholder="Last Name"
+                  value={this.state.lastName}
+                  onChange={this.handleChange}
+                />
               </div>
               <div>
-                <input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" value={this.state.emailAddress} onChange={this.handleChange} />
+                <input
+                  id="emailAddress"
+                  name="emailAddress"
+                  type="text"
+                  className=""
+                  placeholder="Email Address"
+                  value={this.state.emailAddress}
+                  onChange={this.handleChange}
+                />
               </div>
               <div>
-                <input id="password" name="password" type="password" className="" placeholder="Password" value={this.state.password} onChange={this.handleChange} />
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  className=""
+                  placeholder="Password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                />
               </div>
               <div>
-                <input id="confirmPassword" name="confirmPassword" type="password" className="" placeholder="Confirm Password"
-                  value={this.state.confirmPassword} onChange={this.handleChange} />
+                <input
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  type="password"
+                  className=""
+                  placeholder="Confirm Password"
+                  value={this.state.confirmPassword}
+                  onChange={this.handleChange}
+                />
               </div>
-              <div className="grid-100 pad-bottom">
-                <button className="button" type="submit">Sign Up</button>
-                <button className="button button-secondary" onClick={this.cancel}>Cancel</button>
-              </div>
+              <ButtonWrapper className="grid-100 pad-bottom">
+                <button className="button" type="submit">
+                  Sign Up
+                </button>
+                <button
+                  className="button button-secondary"
+                  onClick={this.cancel}
+                >
+                  Cancel
+                </button>
+              </ButtonWrapper>
             </form>
           </div>
           <p>&nbsp;</p>
-          <p>Already have a user account? <Link to="/signin">Click here</Link> to sign in!</p>
-        </div>
+          <p>
+            Already have a user account? <Link to="/signin">Click here</Link> to
+            sign in!
+          </p>
+        </FormWrapper>
       </div>
     );
-  };
+  }
 }
 
 export default UserSignUp;
